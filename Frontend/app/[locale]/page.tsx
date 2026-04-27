7896d6af1114b77f18fc75090bc0787b9a405ac7
FuadTesfaye <fuadtesfaye24@gmail.com>
2026-04-27 19:25:20 +0300

    page cleanups

---
+++ Frontend/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/app/components/Hero";
+import LandingFeatures from "@/app/components/LandingFeatures";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <LandingFeatures />
+    </main>
+  );
+}
